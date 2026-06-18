039bdb131a24bc7c4f2696f8474297a02aaa426f
Igor <zvuash-13@yandex.ru>
2026-06-18 10:39:12 +0000

    Added support for clang-format and clang-tidy tools

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Калькулятор поддерживает следующие действия: сложение, вычитание, умножение, деление и возведение в степень
 
 Для сборки с динамической библиоткой выполнить комадны:
-cmake -B build;
+cmake -B build -DENABLE_CLANG_FORMAT=ON -DENABLE_CLANG_TIDY=ON;
 cmake --build build;
 cmake --build build --target install;
 
--- src/app.cpp
+++ src/app.cpp
@@ -1,4 +1,5 @@
 #include "app.h"
+
 #include "libmath.h"
 
 #include <iostream>
@@ -8,77 +9,79 @@ namespace
 
 struct Task
 {
-	int value1;
-	char operation;
-	int value2;
-	int status;
-	int result;
+    int value1;
+    char operation;
+    int value2;
+    int status;
+    int result;
 };
 
 void parse(int argc, char** argv, Task& task)
 {
-	// TODO Добавить проверку, что argc равно 4
-	task.value1 = std::atoi(argv[1]);
-	task.operation = *(argv[2]);
-	task.value2 = std::atoi(argv[3]);
+    // TODO Добавить проверку, что argc равно 4
+    task.value1 = std::atoi(argv[1]);
+    task.operation = *(argv[2]);
+    task.value2 = std::atoi(argv[3]);
 }
 
 void calculate(Task& task)
 {
-	task.status = 0;
-	switch(task.operation)
-	{
-		case '+':
-			task.result = libmath::addition(task.value1, task.value2);
-			break;
-		case '-':
-			task.result = libmath::subtraction(task.value1, task.value2);
-			break;
-		case '*':
-			task.result = libmath::multiplication(task.value1, task.value2);
-			break;
-		case '/':
-			task.status = libmath::division(task.value1, task.value2, task.result);
-			break;
-		case '^':
-			task.result = libmath::power(task.value1, task.value2);
-			break;
-		default:
-			task.status = 1;
-	}
+    task.status = 0;
+    switch (task.operation)
+    {
+        case '+':
+            task.result = libmath::addition(task.value1, task.value2);
+            break;
+        case '-':
+            task.result = libmath::subtraction(task.value1, task.value2);
+            break;
+        case '*':
+            task.result = libmath::multiplication(task.value1, task.value2);
+            break;
+        case '/':
+            task.status =
+                libmath::division(task.value1, task.value2, task.result);
+            break;
+        case '^':
+            task.result = libmath::power(task.value1, task.value2);
+            break;
+        default:
+            task.status = 1;
+    }
 }
 
 void output(Task task)
 {
-	if (task.status == 0)
-	{
-		std::cout << task.value1 << ' ' << task.operation << ' ' << task.value2 << " = " << task.result << '\n';
-	}
-	else if (task.status == -1)
-	{
-		std::cout << "Error! Division by zero!\n";
-	}
-	else if (task.status == 1)
-	{
-		std::cout << "Error! Unknown operation!\n";
-	}
-	else
-	{
-		std::cout << "Unknown error\n";
-	}
+    if (task.status == 0)
+    {
+        std::cout << task.value1 << ' ' << task.operation << ' ' << task.value2
+                  << " = " << task.result << '\n';
+    }
+    else if (task.status == -1)
+    {
+        std::cout << "Error! Division by zero!\n";
+    }
+    else if (task.status == 1)
+    {
+        std::cout << "Error! Unknown operation!\n";
+    }
+    else
+    {
+        std::cout << "Unknown error\n";
+    }
 }
 
-}
+} // namespace
 
 namespace app
 {
 
 void run(int argc, char** argv)
 {
-	Task task;
-	parse(argc, argv, task);
-	calculate(task);
-	output(task);
+    Task task;
+    parse(argc, argv, task);
+    calculate(task);
+    output(task);
 }
 
-}
+} // namespace app
